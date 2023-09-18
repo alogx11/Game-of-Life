fc6376f0b52400fcb1068587ed9f488e135074b0
alogx11 <alogx11@gmail.com>
2023-09-18 11:31:28 -0500

    added global variables needed to run game

--- sketch.js
+++ sketch.js
@@ -1,1 +1,9 @@
-﻿
+﻿let cellSize = 12;
+let grid, nextGrid;
+let columns, rows;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  columns = Math.floor(windowWidth / cellSize);
+  rows = Math.floor(windowHeight / cellSize);
+}
