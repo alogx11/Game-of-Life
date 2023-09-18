9bf18eb8b1422735d8966f9b0f9b2cb4580ddfe6
alogx11 <alogx11@gmail.com>
2023-09-18 11:40:38 -0500

    added function to create Grid

--- sketch.js
+++ sketch.js
@@ -7,3 +7,16 @@ function setup() {
   columns = Math.floor(windowWidth / cellSize);
   rows = Math.floor(windowHeight / cellSize);
 }
+
+function draw() {}
+function displayGrid() {}
+function createGrid() {
+  grid = new Array(columns);
+  for (let i = 0; i < grid.length; i++) {
+    grid[i] = new Array(rows);
+    grid[i].fill(0);
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = Math.floor(Math.random(0, 2));
+    }
+  }
+}
